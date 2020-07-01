b25eace21faa259b99d40d15b441b9d646d98d3c
valecor95 <coretti.1635747@studenti.uniroma1.it>
2020-07-01 16:47:34 +0200

    Fixed payload reducer

--- preprocessing/payload_reducer.py
+++ preprocessing/payload_reducer.py
@@ -7,7 +7,7 @@ import sys
 
 if len(sys.argv) > 2:
     with jsonlines.open(sys.argv[1]) as reader, jsonlines.open(sys.argv[2], mode='w') as writer:
-        for line in reader:
+        for line in reader: 
             x = {
                 "created_at": line['created_at'],
                 "id": line['id'],
@@ -26,6 +26,25 @@ if len(sys.argv) > 2:
                 },
                 "retweet_count": line['retweet_count'],
             }
+            if('retweeted_status' in line):
+                x['retweeted_status'] = {
+                    "created_at": line['retweeted_status']['created_at'],
+                "id": line['retweeted_status']['id'],
+                "full_text": line['retweeted_status']['full_text'],
+                "entities": {
+                    "hashtags": line['retweeted_status']['entities']['hashtags'],
+                    "symbols": line['retweeted_status']['entities']['symbols'],
+                    "user_mentions": line['retweeted_status']['entities']['user_mentions'],
+                    "urls": line['retweeted_status']['entities']['urls']
+                },
+                "user": {
+                    "id": line['retweeted_status']['user']['id'],
+                    "name": line['retweeted_status']['user']['name'],
+                    "screen_name": line['retweeted_status']['user']['screen_name'],
+                    "location": line['retweeted_status']['user']['location']
+                },
+                "retweet_count": line['retweeted_status']['retweet_count'],
+                }
             writer.write(x)
     print('Done!')
 
